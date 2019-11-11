a26d4c3039568f892b3ff000d9881f7c84260ab9
neeraj rajpurohit <neeraj.rajpurohit@blindside.pro>
2019-11-11 14:34:44 +0530

    choices added

--- api/students/models.py
+++ api/students/models.py
@@ -8,12 +8,18 @@ class User(models.Model):
 
     def __str__(self):
         return self.name
-        
+
 class Complaint(models.Model):
     title = models.CharField(max_length=100)
     content = models.CharField(max_length=3000)
     cuser = models.ForeignKey(User, on_delete=models.CASCADE)
-    status = models.CharField(max_length=10)
+    status = models.CharField(
+        # Add other status choices if required
+        choices = [("submitted", 'submitted'), 
+                        ("under review", "under review"), 
+                            ("resolved", "resolved") ],
+        max_length=20
+    )
     
     def __str__(self):
         return self.title
